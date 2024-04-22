c99469d66fbb5285737a75b7c932a3d5e2ac5113
Annie Sexton <anniesexton@Annies-Laptop.local>
2024-04-22 13:34:36 -0700

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  integrations: [tailwind()],
-  adapter: node({
-    mode: "standalone"
-  })
-});
+export default defineConfig({});
