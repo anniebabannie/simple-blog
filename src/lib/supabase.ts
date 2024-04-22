f44ff22fa3710ba4cf77b1746cc0f19c0c407d1a
Annie Sexton <anniesexton@Annies-Laptop.local>
2024-04-22 16:48:37 -0700

    WIP

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("process.env", process.env.SUPABASE_URL);
+console.log("process.env", typeof process.env.SUPABASE_URL);
 
 const supabaseUrl = process.env.SUPABASE_URL ? process.env.SUPABASE_URL : import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY ? process.env.SUPABASE_ANON_KEY : import.meta.env.SUPABASE_ANON_KEY;
-// const supabaseUrl = import.meta.env.SUPABASE_URL ? import.meta.env.SUPABASE_URL : process.env.SUPABASE_URL;
-// const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY ? import.meta.env.SUPABASE_ANON_KEY : process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl as string,
