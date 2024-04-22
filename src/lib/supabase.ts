50cb9bff297e51dfb60b7c86a73f72917c76a211
Annie Sexton <anniesexton@Annies-Laptop.local>
2024-04-22 16:17:46 -0700

    WIP

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
-
+console.log("import.meta", import.meta);
 const supabaseUrl = import.meta ? import.meta.env.SUPABASE_URL : process.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta ? import.meta.env.SUPABASE_ANON_KEY : process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl as string,
-  supabaseAnonKey,
+  supabaseAnonKey as string,
   {
     auth: {
       flowType: "pkce",
